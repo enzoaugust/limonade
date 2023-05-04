3bfe02945cbdaa70ea465f4790cce647a2f5e44a
Maria Eduarda <125852009+Mariaeduardamatias@users.noreply.github.com>
2023-05-04 11:24:37 -0300

    limonade

--- app/src/main/java/com/example/limonade/MainActivity.kt
+++ app/src/main/java/com/example/limonade/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.limonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,8 +12,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.modifierElementOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.Black
+                    color = Color.White
                 ) {
                     ("Limonade")
                 }
@@ -46,22 +51,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun Limonade {
+fun Limonade() {
     Text(
-        text = "toque no limao",
+        text = "toque no limao 2 vezes",
         textAlign = TextAlign.Center,
         fontSize = 30.sp,
         fontWeight = FontWeight.Bold,
-        modifier = Modifier .fillMaxWidth(),
-        .filMaxWidth()
-        .padding(top= 60.dp)
+        modifier = Modifier .fillMaxWidth()
     )
 
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        vertical
-
-    }
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+     Image(
+         painter = painterResource(id = R.drawable.lemon_tree),
+         contentDescription = null)
+        modifier= Modifier
     }
 }
+I
